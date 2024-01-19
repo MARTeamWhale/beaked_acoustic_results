d8c73a5d6139b1de1b976d3914aab31400f6209e
joystanistreet <joy.stanistreet@dfo-mpo.gc.ca>
2024-01-19 10:40:34 -0400

    small grammatical edits; name output report with date created

--- render_report_word.R
+++ render_report_word.R
@@ -1,8 +1,8 @@
 
 # Helper script to run BWD_report_word.Rmd
 
-library(here)
-library(knitr)
+library(pacman)
+pacman::p_load(here, knitr)
 
 ################################################################################
 
@@ -12,10 +12,10 @@ library(knitr)
 # required:
 
 project = "DFO_MAR"
-deployment = "GBK_2020_09" # name of deployment to summarize
+deployment = "COC_2020_09" # name of deployment to summarize
 metadata = "deployment_summary.csv" # name of metadata csv file
 hourly = FALSE # true if validated on an hourly basis (otherwise assumes daily)
-missing_data = TRUE # true if there is missing data within deployment period
+missing_data = FALSE # true if there is missing data within deployment period
 
 
 # if missing_data = TRUE, specify start and end date(s) of missing data period(s) (if false, this will be ignored)
@@ -24,8 +24,8 @@ missing_data = TRUE # true if there is missing data within deployment period
   # 1) use format c("YYYY-MM-DD", "YYYY-MM-DD")
   # 2) if missing data spans two years, list as two separate periods (ending Dec 31 and starting Jan 1)
 
-missing_data_starts = c("2020-11-16","2021-03-09","2021-05-05")
-missing_data_ends = c("2020-12-24","2021-03-28","2021-05-24")
+missing_data_starts = c(" ")
+missing_data_ends = c(" ")
 
 ################################################################################
 
@@ -33,7 +33,7 @@ missing_data_ends = c("2020-12-24","2021-03-28","2021-05-24")
 
 # define function
 render_BWD_report = function(
-  project, deployment, metadata, hourly, missing_data, missing_data_start, missing_data_end) {
+  project, deployment, metadata, hourly, missing_data, missing_data_starts, missing_data_ends) {
 
   rmarkdown::render(
     "BWD_report_word.Rmd",
@@ -47,9 +47,9 @@ render_BWD_report = function(
         missing_data_ends = missing_data_ends
       ),
     output_file = paste0(r"(R:\\Science\\CetaceanOPPNoise\\CetaceanOPPNoise_4\\PAM_analysis)",
-                         "\\", project, "\\", deployment, "\\", "BWD_Report-", deployment, ".docx")
+                         "\\", project, "\\", deployment, "\\", deployment, "_beaked_whale_summary_", Sys.Date(), ".docx")
   )
 }
 
 # render report based on parameters set above
-render_BWD_report(project, deployment, metadata, hourly, missing_data, missing_data_start, missing_data_end)
+render_BWD_report(project, deployment, metadata, hourly, missing_data, missing_data_starts, missing_data_ends)
